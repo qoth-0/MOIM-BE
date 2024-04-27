cdd98772d08a6970c63994b49e58e9c210d65834
Bellwin98 <jongseunghan@naver.com>
2024-04-27 23:32:58 +0900

    [Feat/Oauth2] 동일 닉네임의 소셜 계정으로 로그인 하면 하나의 Member에 연동(#75)
    * 닉네임 중복 에러 발생하지 않는다.
    * Account Entity 신규 생성
    * Account Entity에는 변동되는 데이터를 포함(이메일, 프로필 사진, socialId, loginType)
    * 일반 회원가입 시 Account와 Member 함께 생성
    * 동일 닉네임 계정으로 로그인 시 Member의 대표 데이터(email, profileImage, loginType이 현재 로그인 한 Account의 데이터로 최신화 됨

---
+++ src/main/java/com/team1/moim/domain/member/entity/LoginType.java
@@ -0,0 +1,5 @@
+package com.team1.moim.domain.member.entity;
+
+public enum LoginType {
+    NORMAL, KAKAO, GOOGLE
+}
--- src/main/java/com/team1/moim/domain/member/entity/SocialType.java
+++
@@ -1,5 +0,0 @@
-package com.team1.moim.domain.member.entity;
-
-public enum SocialType {
-    KAKAO, GOOGLE
-}
