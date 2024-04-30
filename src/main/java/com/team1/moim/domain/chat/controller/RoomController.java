15aded6327fd3855126819d7c4a649601dc3fd2c
JangEunJi <{Jang-Eun-Ji}+{JangEunJi}@users.noreply.github.com>
2024-04-30 17:03:27 +0900

    채팅 리스트 조회

--- src/main/java/com/team1/moim/domain/chat/controller/RoomController.java
+++ src/main/java/com/team1/moim/domain/chat/controller/RoomController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.team1.moim.domain.chat.dto.request.MemberRoomRequest;
 import com.team1.moim.domain.chat.dto.request.RoomRequest;
 import com.team1.moim.domain.chat.dto.response.RoomDetailResponse;
+import com.team1.moim.domain.chat.dto.response.RoomListResponse;
 import com.team1.moim.domain.chat.service.RoomService;
 import com.team1.moim.domain.member.service.MemberService;
 import com.team1.moim.global.dto.ApiSuccessResponse;
@@ -15,10 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -44,5 +42,20 @@ public class RoomController {
                         httpServletRequest.getServletPath(),
                         roomService.create(roomRequest, memberRoomRequests)));
     }
+     // 채팅룸 조회
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/rooms/{pageNum}")
+    public ResponseEntity<ApiSuccessResponse<List<RoomListResponse>>> findAllRoom(
+            HttpServletRequest httpServletRequest,
+            @PathVariable("pageNum") int pageNum){
+        log.info("채팅 조회 시작");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiSuccessResponse.of(
+                        HttpStatus.OK,
+                        httpServletRequest.getServletPath(),
+                        roomService.findAllRoom(pageNum)));
+    }
+
 
 }
